0c79a0d3cff4104b8e93ab4d2fde3530625e5fba
hmmer <hmmer@hmmer.(none)>
2011-12-09 22:09:47 -0800

    Finished example for invoking hmmer3 programmatically without writing query sequence to file.

--- fork_exec.cc
+++ fork_exec.cc
@@ -1,23 +1,43 @@
-// C headers
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
+char sequence[] = ">1UBI:A|PDBID|CHAIN|SEQUENCE\nMQIFVKTLTGKTITLEVEPSDTIENVKAKIQDKEGIPPDQQRLIFAGKQLEDGRTLSDYNIQKESTLHLVLRLRGG";
+
 int main(int argc, char* argv[]) {
+  int descriptors[2];
+  pipe(descriptors);
+
   pid_t pid = fork();
 
   if (pid == 0) {  // child process
-    char executable[] = "/usr/local/bin/phmmer";
-    char query[] = "/home/hmmer/devel/input/1ubi.fasta";
-    char database[] = "/home/hmmer/data/uniprot_sprot.fasta";
+    // Replace stdin with the input part of the pipe
+    dup2(descriptors[0], 0);
 
-    char* const args[] = { executable, query, database, NULL };
+    // Close the unused half of the pipe
+    close(descriptors[1]);
+
+    char executable[] = "/usr/local/bin/phmmer";
+    char database[] = "/home/hmmer/data/uniprot_sprot.fasta";
+    char source[] = "-";
+
+    char* const args[] = { executable, source, database, NULL };
     execvp(executable, args);
     exit(0);
   } else {  // parent process
+    // Replace stdout with the output part of the pipe
+    dup2(descriptors[1], 1);
+
+    // Close the unused part of the pipe
+    close(descriptors[0]);
+
+    char executable[] = "/bin/echo";
+    char* const args[] = { executable, sequence, NULL };
+    execvp(executable, args);
+
     int status;
     wait(&status);
+    close(descriptors[0]);
   }
   
   return 0;
